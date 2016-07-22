49277959e9f30833bb1ef2059f3c465064e4ccb6
Marlon Bernardes <marlonjb.cb@gmail.com>
2016-07-22 14:16:53 +0200

    Change my oh-my-zsh theme to Bullet Train

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -6,10 +6,15 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source=$DIR/zshrc
 target=$HOME/.zshrc
+zshCustomDir=$HOME/.oh-my-zsh/custom/
 
 if [ ! -L "$target" ]; then
   echo "Creating a symbolic link from $source to $target"
   ln -s $source $target
 fi
+
+# Copy all themes to /themes
+mkdir -p $zshCustomDir/themes/
+cp $DIR/themes/* $zshCustomDir/themes/
 
 echo "Oh-My-Zsh setup successful."
